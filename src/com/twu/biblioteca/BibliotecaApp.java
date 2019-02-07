3968c3bfa76d3d01b74bd66b459889708a5154cb
jshen18 <jennshen18@gmail.com>
2019-02-07 14:27:29 -0600

    1.6 User is able to continue using Biblioteca until option to quit

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -11,23 +11,36 @@ public class BibliotecaApp {
 //        reception.greetCustomer();
 //        reception.showBooksToCustomer();
         System.out.println("1: List of Books");
+        System.out.println("2: Choice 2");
+        System.out.println("3: Choice 3");
+        System.out.println("4: Quit Biblioteca");
 
 
         Scanner scanner = new Scanner(System.in);
-        int userInput = scanner.nextInt();
-        switch (userInput) {
-            case 1:
-                reception.showBooksToCustomer();
-                break;
+        int userInput;
+        do {
+            userInput = scanner.nextInt();
+            switch (userInput) {
+                case 1:
+                    reception.showBooksToCustomer();
+                    break;
 
-            case 2:
-                System.out.println("PRINT CASE 2");
-                break;
+                case 2:
+                    System.out.println("choice 2");
+                    break;
+                case 3:
+                    System.out.println("choice 3");
+                    break;
+                case 4:
+                    System.out.println("QUIT");
+                    break;
 
-            default:
-                System.out.println("Please Select a valid option!");
+                default:
+                    System.out.println("Please Select a valid option!");
 
-        }
+            }
+
+        } while (userInput != 4);
 
     }
 
