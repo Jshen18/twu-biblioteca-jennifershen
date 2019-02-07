7baeb87e1cab361ef4d8653889104cf3a6a535d3
jshen18 <jennshen18@gmail.com>
2019-02-07 15:07:24 -0600

    Refactoring options to user to Receptionist class

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -8,39 +8,9 @@ public class BibliotecaApp {
     public static void main(String[] args) {
 
         Receptionist reception = new Receptionist();
-//        reception.greetCustomer();
-//        reception.showBooksToCustomer();
-        System.out.println("1: List of Books");
-        System.out.println("2: Choice 2");
-        System.out.println("3: Choice 3");
-        System.out.println("4: Quit Biblioteca");
+        reception.greetCustomer();
+        reception.showOptionsToCustomer();
 
-
-        Scanner scanner = new Scanner(System.in);
-        int userInput;
-        do {
-            userInput = scanner.nextInt();
-            switch (userInput) {
-                case 1:
-                    reception.showBooksToCustomer();
-                    break;
-
-                case 2:
-                    System.out.println("choice 2");
-                    break;
-                case 3:
-                    System.out.println("choice 3");
-                    break;
-                case 4:
-                    System.out.println("QUIT");
-                    break;
-
-                default:
-                    System.out.println("Please Select a valid option!");
-
-            }
-
-        } while (userInput != 4);
 
     }
 
--- src/com/twu/biblioteca/Receptionist.java
+++ src/com/twu/biblioteca/Receptionist.java
@@ -2,6 +2,7 @@ package com.twu.biblioteca;
 
 import java.io.PrintStream;
 import java.util.List;
+import java.util.Scanner;
 
 public class Receptionist {
 
@@ -9,6 +10,41 @@ public class Receptionist {
 
     public void greetCustomer() {
         System.out.println("Welcome to Biblioteca. Your one stop-shop for great book titles in Bangalore!");
+    }
+
+    public void showOptionsToCustomer() {
+
+        System.out.println("1: List of Books");
+        System.out.println("2: Choice 2");
+        System.out.println("3: Choice 3");
+        System.out.println("4: Quit Biblioteca");
+
+        Scanner scanner = new Scanner(System.in);
+        int userInput;
+        do {
+            userInput = scanner.nextInt();
+            switch (userInput) {
+                case 1:
+                    this.showBooksToCustomer();
+                    break;
+
+                case 2:
+                    System.out.println("choice 2");
+                    break;
+                case 3:
+                    System.out.println("choice 3");
+                    break;
+                case 4:
+                    System.out.println("QUIT");
+                    break;
+
+                default:
+                    System.out.println("Please Select a valid option!");
+
+            }
+
+        } while (userInput != 4);
+
     }
 
     public void showBooksToCustomer() {
