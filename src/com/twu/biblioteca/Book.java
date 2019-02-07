94e98493f36b22ad584dd43207a0363d31018fc2
Jennifer Shen <jennifershen@JenniferShen-Mac.local>
2019-02-06 22:36:51 -0600

    Add column separators on book list

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Book {
 
-    String[] books = new String[]{"Harry Potter and the Chamber of Secrets by J.K. Rowling 2005\n", "The Water Cure by Sophie Mackintosh 2019\n", "Black Leopard, Red Wolf by Marlon James 2019\n", "Where Reasons End by Yiyun Li 2018"};
+    String[] books = new String[]{"Harry Potter and the Chamber of Secrets | J.K. Rowling | 2005\n", "The Water Cure |  Sophie Mackintosh | 2019\n", "Black Leopard, Red Wolf |  Marlon James |  2019\n", "Where Reasons End |  Yiyun Li | 2018"};
 
     public String[] getBooks() {
         return books;
